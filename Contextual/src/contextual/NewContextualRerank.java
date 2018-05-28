4462259fcf72b560dbdc3d2410eabadb01d24917
luciano <lucianoadfilho@gmail.com>
2018-05-28 12:30:00 -0300

    new experiment started

---
+++ Contextual/src/contextual/NewContextualRerank.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package contextual;
+
+import util.Distbin;
+import util.Rank;
+
+/**
+ *
+ * @author Luciano Araujo Dourado Filho
+ */
+public class NewContextualRerank {
+    
+    public static Rank getRank(Distbin d){
+        return null;
+    }
+        
+}
