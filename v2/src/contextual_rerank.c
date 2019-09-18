eb284b4580ded30d1de6cf7370c2a488cf5b13da
FalsoMoralista <lucianoadfilho@gmail.com>
2019-09-18 12:47:15 -0300

    updating v2

---
+++ v2/src/contextual_rerank.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+*   Build an image's kNN given its amount of neightbors
+*   Params: 
+*   K: amount of neighbors
+*   filter: image id
+**/
+int build_kNN(int K, int filter){
+    int kNN[K];
+    int pos = 0;
+    int aux = 1;
+    while (pos<K)
+    {
+        /* code */
+    }
+    
+    return kNN;    
+}
+
