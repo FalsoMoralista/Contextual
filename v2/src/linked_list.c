ae7a49db97edbb7f2a6c03f970f1f978a9668893
FalsoMoralista <lucianoadfilho@gmail.com>
2019-09-19 21:00:24 -0300

    gaved up on trying to create a generic linked list

--- v2/src/linked_list.c
+++ v2/src/linked_list.c
@@ -1,65 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct Node{
-    void *data;
+typedef struct Node {
+    double data;
     struct Node *next;
 }Node;
 
 
-typedef struct List{
+typedef struct List {
     int size;
     struct Node *head;  
 }List;
 
 
-void AddElement(Node *n, List *l){
+
+void AddElement(double d, List *l) {
+    Node *n = (Node*)malloc(sizeof(Node));    
+    n->data = d;
+    n->next = NULL;
+//    printf("valor:%lf\n",*(double*)n->data);
+
+
     if (l->size==0){      
         l->head = n;
-        l->head->next = NULL; 
-        l->size += 1;
     }else {
-        Node *aux = (Node*)malloc(sizeof(Node));        
         Node *ant;
+        Node *aux;
         ant = l->head;
-        aux = l->head->next;
+        aux = l->head;
         while(aux != NULL){
             ant = aux;
             aux = aux->next;        
         }
-        n->next = NULL;
         ant->next = n;
-        l->size += 1;
     }    
+        l->size += 1;
 }
 
-static List *NewList(){
+static List *NewList() {
     List *l = (List*)malloc(sizeof(List));
     l->head = NULL;
     l->size = 0;
     return l;
 }
 
-void print(List *l){
-        Node *aux = (Node*)malloc(sizeof(Node));        
+void print(List *l) {
+        Node *aux;        
         Node *ant;
         aux = l->head;
         while(aux != NULL){
-            printf("%lf\n",*(double*)aux->data);
-            ant = aux;
+            printf("Data = %lf\n",(double)aux->data);
             aux = aux->next;        
         }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     List *l = NewList();
-    for (int ctr = 0; ctr < 5; ctr++) {
-        Node *n = (Node*)malloc(sizeof(Node));
-        double d = 1.3352 + ctr;
-        n->data = &d;
-        AddElement(n,l);   
-    }
+    for (int ctr = 0; ctr < 100; ctr++) {
+        double d = 1.0342 + ctr;
+        AddElement(d,l);   
+    }   
     print(l);
     printf("Tamanho: %d\n",l->size);
     return 0;
