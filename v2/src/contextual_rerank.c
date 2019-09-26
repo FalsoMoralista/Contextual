6fbba34ca10bae3d1fb610284f7bc9ff8c4daa39
FalsoMoralista <lucianoadfilho@gmail.com>
2019-09-26 11:45:14 -0300

    update

--- v2/src/contextual_rerank.c
+++ v2/src/contextual_rerank.c
@@ -11,16 +11,20 @@
 *   an other image, we need to filter both of them from the kNN set
 *   that is going to be built. Therefore 
 **/
-int *build_kNN(int K, int imgI, int imgJ, Rank r) {
-    int *kNN = malloc(K*sizeof(int));
+int *build_kNN(int K, int imgI, int imgJ, Rank r)
+{
+    int *kNN = malloc(K * sizeof(int));
     int pos = 0;
     int aux = 0;
-    if (r.entries[aux].id == imgI) {
+    if (r.entries[aux].id == imgI)
+    {
         aux++;
     }
-    while (pos < K){
+    while (pos < K)
+    {
         int tmp = r.entries[aux].id;
-        if (tmp == imgJ){
+        if (tmp == imgJ)
+        {
             aux++;
         }
         kNN[pos++] = r.entries[aux++].id;
@@ -28,31 +32,71 @@ int *build_kNN(int K, int imgI, int imgJ, Rank r) {
     return kNN;
 }
 
-int main(int argc, char const *argv[])
+void ContextualRerank(Distbin original_distances[20180], Rank *ranked_lists, int Ks, int Ke)
 {
-    FILE *pointer;
-    pointer = fopen("../../Contextual/resources/images/16672.jpg.ppm.distbin", "r");
-    if (!pointer)
+    int K = Ks;
+    while (K < Ke)
     {
-        printf("Unable to access \n");
-    }
-    else
-    {
-        Distbin *distbin = NewDistbin(pointer, distbin);
-        printf("˜here we go again˜\n");
-        Rank r = NewRank(distbin);
-        for (int i = 0; i < 8; i++)
-        {
-            printf("Distance:%lf\n", r.entries[i].distance);
-            printf("Id: %d\n", r.entries[i].id);
-        }
-        int *knn = build_kNN(7,16672,9739,r);
-        for (int i = 0; i < 7; i++)
-        {
-            printf("Id knn[%d](img 16672): %d\n",i,knn[i]);
-        }
-        
+        for (int imgI = 20000; imgI < 20180; imgI++)
+        { // for each imgI
 
+            for (int imgL = 0; imgL < 20000; imgL++)
+            { // for each imgL (topics rows)
+
+                if (imgI != imgL)
+                { // discard itself
+
+                    //                String currentIMG = clef.getProperty(Integer.toString(i)); // get the current image
+
+                    //                File f = new File(DATA_DIRECTORY + dScriptor + "/" + currentIMG + EXT);
+
+                    //                Distbin distbin = new Distbin(COLLECTION_SIZE - 180, f);
+
+                    Rank rank = ranked_lists[imgI - 20000];
+
+                    int kNN = buildKNN(K,imgI,imgL, rank);
+
+                    int ck = 0;
+                    double dj = 0;
+                    for (int j = 0; j < K ; j++)
+                    { // for each imgJ((KNN)L) do : weighted sum of distance from imgL neighbors, to imgI
+                        dj = dj + original_distances[imgL].distances[j] * (K - ck);
+                        ck++;
+                    }
+                    double di = original_distances[imgI].distances[imgL] / K;
+                    dj = dj / (K * (K + 1) / 2);
+                    di = Math.pow(di, 2);
+                    dj = Math.pow(dj, 2);
+
+                    topics[i][l - 20000] = Math.sqrt(di + dj); // recalculate distance from imgI to imgL
+                }
+            }
+        }
     }
-    return 0;
-}
+
+    int main(int argc, char const *argv[])
+    {
+        FILE *pointer;
+        pointer = fopen("../../Contextual/resources/images/16672.jpg.ppm.distbin", "r");
+        if (!pointer)
+        {
+            printf("Unable to access \n");
+        }
+        else
+        {
+            Distbin *distbin = NewDistbin(pointer, distbin);
+            printf("˜here we go again˜\n");
+            Rank r = NewRank(distbin);
+            for (int i = 0; i < 8; i++)
+            {
+                printf("Distance:%lf\n", r.entries[i].distance);
+                printf("Id: %d\n", r.entries[i].id);
+            }
+            int *knn = build_kNN(7, 16672, 9739, r);
+            for (int i = 0; i < 7; i++)
+            {
+                printf("Id knn[%d](img 16672): %d\n", i, knn[i]);
+            }
+        }
+        return 0;
+    }
